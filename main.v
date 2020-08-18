e81c8e3a5cd4804006130eb030be5e3c88b56e44
spaceface777 <spaceface7777@gmail.com>
2020-08-18 19:41:27 +0200

    A whole bunch of changes
    - Android / wasm support
    - Tiles drawn natively using rectangles
    - Control using touchscreen swipes
       - Support for multiple color themes
       - Support for multiple tile text formats
    - Automatic size adjustment when window is resized
    - Probably more I've forgotten

--- main.v
+++ main.v
@@ -2,12 +2,36 @@ import gg
 import gx
 import os
 import rand
-import sokol.sapp
+
+struct App {
+mut:
+	gg            &gg.Context
+	touch         TouchInfo
+	ui            Ui = Ui{}
+	theme         &Theme = themes[0]
+	theme_idx     int = 0
+	// victory_image gg.Image
+	board         Board
+	undo          []Board
+	atickers      [4][4]int
+	state         GameState = .play
+	tile_format   TileFormat = .normal
+	moves         int
+}
+
+struct Ui {
+mut:
+	tile_size     int = 100
+	border_size   int = 32
+	padding_size  int = 16
+	header_size   int = 16
+	font_size     int = 54
+	window_width  int = 544
+	window_height int = 560
+}
 
 struct Tile {
-	id      int
-	points  int
-	picname string
+	color   gx.Color
 }
 
 struct Pos {
@@ -26,97 +50,11 @@ struct ImageLabel {
 	dim Pos
 }
 
-const (
-	window_title        = 'v2048'
-	window_width        = 562
-	window_height       = 562
-	points_label        = TextLabel{
-		text: 'Points: '
-		pos: Pos{10, 5}
-		cfg: gx.TextCfg{
-			align: gx.align_left
-			size: 24
-			color: gx.rgb(0, 0, 0)
-		}
-	}
-	moves_label         = TextLabel{
-		text: 'Moves: '
-		pos: Pos{window_width - 160, 5}
-		cfg: gx.TextCfg{
-			align: gx.align_left
-			size: 24
-			color: gx.rgb(0, 0, 0)
-		}
-	}
-	game_over_label     = TextLabel{
-		text: 'Game Over'
-		pos: Pos{80, 220}
-		cfg: gx.TextCfg{
-			align: gx.align_left
-			size: 100
-			color: gx.rgb(0, 0, 255)
-		}
-	}
-	victory_image_label = ImageLabel{
-		pos: Pos{80, 220}
-		dim: Pos{430, 130}
-	}
-	all_tiles           = [
-		Tile{0, 0, '1.png'},
-		Tile{1, 2, '2.png'},
-		Tile{2, 4, '4.png'},
-		Tile{3, 8, '8.png'},
-		Tile{4, 16, '16.png'},
-		Tile{5, 32, '32.png'},
-		Tile{6, 64, '64.png'},
-		Tile{7, 128, '128.png'},
-		Tile{8, 256, '256.png'},
-		Tile{9, 512, '512.png'},
-		Tile{10, 1024, '1024.png'},
-		Tile{11, 2048, '2048.png'},
-		Tile{12, 4096, '4096.png'},
-		Tile{13, 8196, '8196.png'},
-	]
-)
-
-struct TileImage {
-	tile  Tile
-mut:
-	image gg.Image
-}
-
-// TODO: remove the .str() method here. It is just to prevent C compilation errors
-// about sg_image_str
-fn (t &TileImage) str() string {
-	return 'TileImage{ Tile{id: $t.tile.id, points: $t.tile.points, picname: $t.tile.picname } }'
-}
-
-//
 struct Board {
 mut:
 	field  [4][4]int
 	points int
 	shifts int
-}
-
-fn (b Board) transpose() Board {
-	mut res := b
-	for y := 0; y < 4; y++ {
-		for x := 0; x < 4; x++ {
-			res.field[y][x] = b.field[x][y]
-		}
-	}
-	return res
-}
-
-fn (b Board) hmirror() Board {
-	mut res := b
-	for y := 0; y < 4; y++ {
-		for x := 0; x < 4; x++ {
-			res.field[y][x] = b.field[y][4 - x - 1]
-		}
-	}
-	return res
 }
 
 struct TileLine {
@@ -127,18 +65,53 @@ mut:
 	shifts int
 }
 
-fn no_newlines(s string) string {
-	return s.replace('\n', ' ')
+struct TouchInfo {
+mut:
+	start_pos Pos
 }
 
-//
+enum TileFormat {
+	normal
+	log
+	exponent
+	shifts
+	none_
+	end_ // To know when to wrap around
+}
+
+enum GameState {
+	play
+	over
+	victory
+}
+
+fn (b Board) transpose() Board {
+	mut res := b
+	for y in 0..4 {
+		for x in 0..4 {
+			res.field[y][x] = b.field[x][y]
+		}
+	}
+	return res
+}
+
+fn (b Board) hmirror() Board {
+	mut res := b
+	for y in 0..4 {
+		for x in 0..4 {
+			res.field[y][x] = b.field[y][3 - x]
+		}
+	}
+	return res
+}
+
 fn (t TileLine) to_left() TileLine {
 	right_border_idx := 5
 	mut res := t
 	mut zeros := 0
 	mut nonzeros := 0
 	// gather meta info about the line:
-	for x := 0; x < 4; x++ {
+	for x in 0..4 {
 		if res.field[x] == 0 {
 			zeros++
 		} else {
@@ -174,68 +147,32 @@ fn (t TileLine) to_left() TileLine {
 			}
 			res.shifts++
 			res.field[x]++
-			res.points += all_tiles[res.field[x]].points
+			res.points += 1 << res.field[x]
 		}
 	}
-	// eprintln('TileLine.to_left shifts: $res.shifts | zeros: $zeros | nonzeros: $nonzeros\n${no_newlines(t.str())}\n${no_newlines(res.str())}\n-----------------------------------')
 	return res
 }
 
 fn (b Board) to_left() Board {
 	mut res := b
-	for y := 0; y < 4; y++ {
+	for y in 0..4 {
 		mut hline := TileLine{y}
-		for x := 0; x < 4; x++ {
+		for x in 0..4 {
 			hline.field[x] = b.field[y][x]
 		}
 		reshline := hline.to_left()
 		res.shifts += reshline.shifts
 		res.points += reshline.points
-		for x := 0; x < 4; x++ {
+		for x in 0..4 {
 			res.field[y][x] = reshline.field[x]
 		}
 	}
 	return res
 }
 
-//
-enum GameState {
-	play
-	over
-	victory
-}
-
-struct App {
-mut:
-	gg            &gg.Context
-	tiles         []TileImage
-	victory_image gg.Image
-	//
-	board         Board
-	undo          []Board
-	atickers      [4][4]int
-	state         GameState = .play
-	moves         int
-}
-
-fn (mut app App) new_tile(t Tile) TileImage {
-	mut timage := TileImage{
-		tile: t
-	}
-	timage.image = app.gg.create_image(os.resource_abs_path(os.join_path('assets', t.picname)))
-	return timage
-}
-
-fn (mut app App) load_tiles() {
-	for t in all_tiles {
-		app.tiles << app.new_tile(t)
-	}
-	// eprintln('tiles: $app.tiles')
-}
-
 fn (mut app App) update_tickers() {
-	for y := 0; y < 4; y++ {
-		for x := 0; x < 4; x++ {
+	for y in 0..4 {
+		for x in 0..4 {
 			mut old := app.atickers[y][x]
 			if old > 0 {
 				old--
@@ -245,44 +182,10 @@ fn (mut app App) update_tickers() {
 	}
 }
 
-fn (app &App) draw() {
-	app.draw_tiles()
-	app.gg.draw_text(points_label.pos.x, points_label.pos.y, '$points_label.text ${app.board.points:08}',
-		points_label.cfg)
-	app.gg.draw_text(moves_label.pos.x, moves_label.pos.y, '$moves_label.text ${app.moves:5d}',
-		moves_label.cfg)
-	if app.state == .over {
-		app.gg.draw_text(game_over_label.pos.x, game_over_label.pos.y, game_over_label.text,
-			game_over_label.cfg)
-	}
-	if app.state == .victory {
-		app.gg.draw_image(victory_image_label.pos.x, victory_image_label.pos.y, victory_image_label.dim.x,
-			victory_image_label.dim.y, app.victory_image)
-	}
-}
-
-fn (app &App) draw_tiles() {
-	border := 10
-	xstart := 10
-	ystart := 30
-	tsize := 128
-	for y := 0; y < 4; y++ {
-		for x := 0; x < 4; x++ {
-			tidx := app.board.field[y][x]
-			tile := app.tiles[tidx]
-			tw := tsize - 10 * app.atickers[y][x]
-			th := tsize - 10 * app.atickers[y][x]
-			app.gg.draw_image(xstart + x * (tsize + border) + (tsize - tw) / 2, ystart +
-				y * (tsize + border) + (tsize - th) /
-				2, tw, th, tile.image)
-		}
-	}
-}
-
 fn (mut app App) new_game() {
 	app.board = Board{}
-	for y := 0; y < 4; y++ {
-		for x := 0; x < 4; x++ {
+	for y in 0..4 {
+		for x in 0..4 {
 			app.board.field[y][x] = 0
 			app.atickers[y][x] = 0
 		}
@@ -295,8 +198,8 @@ fn (mut app App) new_game() {
 }
 
 fn (mut app App) check_for_victory() {
-	for y := 0; y < 4; y++ {
-		for x := 0; x < 4; x++ {
+	for y in 0..4 {
+		for x in 0..4 {
 			fidx := app.board.field[y][x]
 			if fidx == 11 {
 				app.victory()
@@ -309,8 +212,8 @@ fn (mut app App) check_for_victory() {
 fn (mut app App) check_for_game_over() {
 	mut zeros := 0
 	mut remaining_merges := 0
-	for y := 0; y < 4; y++ {
-		for x := 0; x < 4; x++ {
+	for y in 0..4 {
+		for x in 0..4 {
 			fidx := app.board.field[y][x]
 			if fidx == 0 {
 				zeros++
@@ -336,13 +239,14 @@ fn (mut app App) check_for_game_over() {
 }
 
 fn (mut app App) new_random_tile() {
-	mut etiles := [16]Pos
+	mut etiles := [16]Pos{}
 	mut empty_tiles_max := 0
-	for y := 0; y < 4; y++ {
-		for x := 0; x < 4; x++ {
+	for y in 0..4 {
+		for x in 0..4 {
 			fidx := app.board.field[y][x]
 			if fidx == 0 {
 				etiles[empty_tiles_max] = Pos{x, y}
+				app.atickers[y][x] = 0
 				empty_tiles_max++
 			}
 		}
@@ -350,9 +254,11 @@ fn (mut app App) new_random_tile() {
 	if empty_tiles_max > 0 {
 		new_random_tile_index := rand.intn(empty_tiles_max)
 		empty_pos := etiles[new_random_tile_index]
-		random_value := 1 + rand.intn(2)
+		// 1/8 chance of creating a `4` tile
+		rint := rand.intn(8)
+		random_value := if rint == 0 { 2 } else { 1 }
 		app.board.field[empty_pos.y][empty_pos.x] = random_value
-		app.atickers[empty_pos.y][empty_pos.x] = 30
+		app.atickers[empty_pos.y][empty_pos.x] = 15
 		// eprintln('>>>>> new_random_tile, app.board.points: $app.board.points | random_value: $random_value at ${no_newlines(empty_pos.str())}')
 	}
 	app.check_for_victory()
@@ -367,64 +273,19 @@ fn (mut app App) game_over() {
 	app.state = .over
 }
 
-type BoardMoveFN fn(b Board) Board
-fn (mut app App) move(move_fn BoardMoveFN) {
+fn (mut app App) move(d Direction) {
 	old := app.board
-	new := move_fn(old)
+	new := match d {
+		.left  { old.to_left() }
+		.right { old.hmirror().to_left().hmirror() }
+		.up    { old.transpose().to_left().transpose() }
+		.down  { old.transpose().hmirror().to_left().hmirror().transpose() }
+	}
 	if old.shifts != new.shifts {
 		app.moves++
 		app.board = new
 		app.undo << old
 		app.new_random_tile()
-	}
-}
-
-fn (mut app App) on_key_down(key sapp.KeyCode) {
-	// these keys are independent from the game state:
-	match key {
-		.escape {
-			exit(0)
-		}
-		.space {
-			app.new_random_tile()
-		}
-		.n {
-			app.new_game()
-		}
-		.backspace {
-			if app.undo.len > 0 {
-				app.state = .play
-				app.board = app.undo.pop()
-				app.moves--
-				return
-			}
-		}
-		else {}
-	}
-	if app.state == .play {
-		match key {
-			.up, .w { app.move(fn (b Board) Board {
-					return b.transpose().to_left().transpose()
-				}) }
-			.left, .a { app.move(fn (b Board) Board {
-					return b.to_left()
-				}) }
-			.down, .s { app.move(fn (b Board) Board {
-					return b.transpose().hmirror().to_left().hmirror().transpose()
-				}) }
-			.right, .d { app.move(fn (b Board) Board {
-					return b.hmirror().to_left().hmirror()
-				}) }
-			else {}
-		}
-	}
-	// eprintln('app.board.points: $app.board.points')
-}
-
-//
-fn on_event(e &sapp.Event, mut app App) {
-	if e.typ == .key_down {
-		app.on_key_down(e.key_code)
 	}
 }
 
@@ -435,26 +296,54 @@ fn frame(mut app App) {
 	app.gg.end()
 }
 
+// TODO: Move this somewhere else once Android support is merged
+$if android {
+	#include <android/log.h>
+	#define LOG_TAG "v_logcat_test"
+	#define printf(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+	#define fprintf(a, ...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+}
+
+
+// Utility functions, to avoid importing `math`
+[inline]
+fn min(a, b int) int {
+	if a < b { return a } else { return b }
+}
+
+[inline]
+fn max(a, b int) int {
+	if a > b { return a } else { return b }
+}
+
+[inline]
+fn abs(a int) int {
+	if a < 0 { return -a } else { return a }
+}
+
 fn main() {
-	mut app := &App{
-		gg: 0
-		state: .play
-	}
+	println('\n'.repeat(30))
+	mut app := &App{ gg: 0 }
 	app.new_game()
+
+	mut font_path := os.resource_abs_path(os.join_path('assets', 'RobotoMono-Regular.ttf'))
+	$if android {
+		font_path = 'RobotoMono-Regular.ttf'
+	}
+
 	app.gg = gg.new_context({
-		bg_color: gx.white
-		width: window_width
-		height: window_height
-		use_ortho: true
+		bg_color: app.theme.bg_color
+		width: default_window_width
+		height: default_window_height
+		use_ortho: false
 		create_window: true
-		window_title: window_title
+		window_title: 'canvas'
 		frame_fn: frame
 		event_fn: on_event
 		user_data: app
-		font_path: os.resource_abs_path(os.join_path('assets', 'RobotoMono-Regular.ttf'))
+		font_path: font_path
 	})
-	app.load_tiles()
-	app.victory_image = app.gg.create_image(os.resource_abs_path(os.join_path('assets',
-		'victory.png')))
+	// app.victory_image = app.gg.create_image(os.resource_abs_path(os.join_path('assets',
+	// 	'victory.png')))
 	app.gg.run()
 }
---
+++ window.v
@@ -0,0 +1,312 @@
+import sokol.sapp
+import gx
+
+struct Theme {
+	bg_color        gx.Color
+	padding_color   gx.Color
+	text_color      gx.Color
+	game_over_color gx.Color
+	victory_color   gx.Color
+	tile_colors     []gx.Color
+}
+
+const (
+	themes = [
+		&Theme {
+			bg_color: gx.rgb(220, 220, 220)
+			padding_color: gx.rgb(143, 130, 119)
+			victory_color: gx.rgb(100, 160, 100)
+			game_over_color: gx.rgb(190, 50, 50)
+			text_color: gx.black
+			tile_colors: [
+				gx.rgb(205, 193, 180) // Empty / 0 tile
+				gx.rgb(238, 228, 218) // 2
+				gx.rgb(237, 224, 200) // 4
+				gx.rgb(242, 177, 121) // 8
+				gx.rgb(245, 149, 99)  // 16
+				gx.rgb(246, 124, 95)  // 32
+				gx.rgb(246, 94, 59)   // 64
+				gx.rgb(237, 207, 114) // 128
+				gx.rgb(237, 204, 97)  // 256
+				gx.rgb(237, 200, 80)  // 512
+				gx.rgb(237, 197, 63)  // 1024
+				gx.rgb(237, 194, 46)  // 2048
+			]
+		},
+		&Theme {
+			bg_color: gx.rgb(55, 55, 55)
+			padding_color: gx.rgb(68, 60, 59)
+			victory_color: gx.rgb(100, 160, 100)
+			game_over_color: gx.rgb(190, 50, 50)
+			text_color: gx.white
+			tile_colors: [
+				gx.rgb(123, 115, 108) // Empty / 0 tile
+				gx.rgb(142, 136, 130) // 2
+				gx.rgb(142, 134, 120) // 4
+				gx.rgb(145, 106, 72) // 8
+				gx.rgb(147, 89, 59)  // 16
+				gx.rgb(147, 74, 57)  // 32
+				gx.rgb(147, 56, 35)   // 64
+				gx.rgb(142, 124, 68) // 128
+				gx.rgb(142, 122, 58)  // 256
+				gx.rgb(142, 120, 48)  // 512
+				gx.rgb(142, 118, 37)  // 1024
+				gx.rgb(142, 116, 27)  // 2048
+			]
+		}, 
+	]
+	window_title          = 'V 2048'
+	default_window_width  = 544
+	default_window_height = 560
+)
+
+enum LabelKind {
+	points
+	moves
+	tile
+	victory
+	game_over
+}
+
+enum Direction {
+	up
+	down
+	left
+	right
+}
+
+const (
+	min_swipe_distance = 100
+)
+
+fn (app &App) label_format(kind LabelKind) gx.TextCfg {
+	match kind {
+		.points {
+			return {
+				color: app.theme.text_color
+				align: .left
+				size: app.ui.font_size / 2
+			}
+		} .moves {
+			return {
+				color: app.theme.text_color
+				align: .right
+				size: app.ui.font_size / 2
+			}
+		} .tile {
+			return {
+				color: app.theme.text_color
+				align: .center
+				vertical_align: .middle
+				size: app.ui.font_size
+			}
+		} .victory {
+			return {
+				color: app.theme.victory_color
+				align: .center
+				vertical_align: .middle
+				size: app.ui.font_size * 2
+			}
+		} .game_over {
+			return {
+				color: app.theme.game_over_color
+				align: .center
+				vertical_align: .middle
+				size: app.ui.font_size * 2
+			}
+		}
+	}
+}
+
+fn (mut app App) set_theme(idx int) {
+	theme := themes[idx]
+	app.theme_idx = idx
+	app.theme = theme
+	app.gg.set_bg_color(theme.bg_color)
+}
+
+fn (mut app App) resize() {
+	w := sapp.width()
+	h := sapp.height()
+	m := f32(min(w, h))
+
+	app.ui.window_width = w
+	app.ui.window_height = h
+	app.ui.padding_size = int(m * 0.03)
+	app.ui.header_size = app.ui.padding_size
+
+	app.ui.border_size = app.ui.padding_size * 2
+	app.ui.tile_size = int((m - app.ui.padding_size * 5 - app.ui.border_size * 2) / 4)
+	app.ui.font_size = int(m / 10)
+
+	// Mobile view: undo, theme, etc. buttons along the bottom
+	if f32(h) / f32(w) > 1.5 {
+		
+	}
+}
+
+fn (app &App) draw() {
+	ww := app.ui.window_width
+	wh := app.ui.window_height
+	labelx := app.ui.border_size
+	labely := app.ui.border_size / 2
+
+	app.draw_tiles()
+	app.gg.draw_text(labelx, labely, 'Points: $app.board.points', app.label_format(.points))
+	app.gg.draw_text(min(ww, wh) - labelx, labely, 'Moves: $app.moves', app.label_format(.moves))
+
+	if app.state == .over {
+		// app.gg.draw_rect(0, 0, ww, wh, gx.rgba(0, 0, 0, 44)) // TODO: Make transparency work
+		app.gg.draw_text(ww / 2, wh / 2, 'Game Over', app.label_format(.game_over))
+	}
+	if app.state == .victory {
+		// app.gg.draw_rect(0, 0, ww, wh, gx.rgba(0, 0, 0, 44)) // TODO: Make transparency work
+		app.gg.draw_text(ww / 2, wh / 2, 'Victory!', app.label_format(.victory))
+
+	}
+}
+
+fn (app &App) draw_tiles() {
+	// println(app.board.fieldtile_s)
+	xstart := app.ui.border_size
+	ystart := app.ui.border_size + app.ui.header_size
+	toffset := app.ui.tile_size + app.ui.padding_size
+	tiles_size := min(app.ui.window_width, app.ui.window_height) - app.ui.border_size * 2
+
+	// Padding around the tiles
+	app.gg.draw_rect(xstart, ystart, tiles_size, tiles_size, app.theme.padding_color)
+
+	// The actual tiles
+	for y in 0..4 {
+		for x in 0..4 {
+			tidx := app.board.field[y][x]
+			tile_color := app.theme.tile_colors[tidx]
+			tw := int(f32(app.ui.tile_size) / 14.5 * (15 - app.atickers[y][x]))
+			th := tw // square tiles, w == h
+			xoffset := xstart + app.ui.padding_size + (x) * toffset + (app.ui.tile_size - tw) / 2
+			yoffset := ystart + app.ui.padding_size + (y) * toffset + (app.ui.tile_size - th) / 2
+			app.gg.draw_rect(xoffset, yoffset, tw, th, tile_color)
+			// If tile is not empty
+			if tidx != 0 {
+				xpos := xoffset + tw / 2
+				ypos := yoffset + th / 2
+
+				fmt := app.label_format(.tile)
+
+				match app.tile_format {
+					.normal {
+						app.gg.draw_text(xpos, ypos, '${1 << tidx}', fmt)
+					}
+					.log {
+						app.gg.draw_text(xpos, ypos, '$tidx', fmt)
+					}
+
+					.exponent {
+						app.gg.draw_text(xpos, ypos, '2', fmt)
+						fs2 := app.ui.font_size * 2 / 3
+						app.gg.draw_text(xpos + app.ui.tile_size / 10, ypos - app.ui.tile_size / 8, '$tidx', { fmt | size: fs2, align: gx.HorizontalAlign.left })
+					}
+					.shifts {
+						fs2 := app.ui.font_size * 2 / 3
+						app.gg.draw_text(xpos, ypos, '2<<${tidx - 1}', { fmt | size: fs2 })
+					}
+					.none_  {} // Don't draw any text here, colors only
+					.end_ {} // Should never get here
+				}
+			}
+		}
+	}
+}
+
+fn on_event(e &sapp.Event, mut app App) {
+	// println(e)
+	match e.typ {
+		.key_down {
+			app.on_key_down(e.key_code)
+		} .resized, .restored, .resumed {
+			app.resize()
+		} .touches_began {
+			if e.num_touches > 0 {
+				t := e.touches[0]
+				app.touch.start_pos = { x: int(t.pos_x), y: int(t.pos_y) }
+				// println(app.touch)
+			}
+		} .touches_ended {
+			if e.num_touches > 0 {
+				t := e.touches[0]
+				end_pos := Pos{ x: int(t.pos_x), y: int(t.pos_y) }
+				// println(app.touch)
+				// println(' => $end_pos')
+				app.handle_swipe(app.touch.start_pos, end_pos)
+			}
+		}
+
+		else {}
+	}
+}
+
+fn (mut app App) on_key_down(key sapp.KeyCode) {
+	// these keys are independent from the game state:
+	match key {
+		.escape {
+			exit(0)
+		}
+		.space {
+			app.new_random_tile()
+		}
+		.n {
+			app.new_game()
+		}
+		.backspace {
+			if app.undo.len > 0 {
+				app.state = .play
+				app.board = app.undo.pop()
+				app.moves--
+				return
+			}
+		}
+		.enter {
+			app.tile_format = int(app.tile_format) + 1
+			if app.tile_format == .end_ {
+				app.tile_format = .normal
+			}
+		}
+		.j {
+			app.game_over()
+		}
+		.t {
+			app.set_theme(if app.theme_idx == themes.len - 1 { 0 } else { app.theme_idx + 1 })
+		}
+		else {}
+	}
+	if app.state == .play {
+		match key {
+			.w, .up    { app.move(.up) }
+			.a, .left  { app.move(.left) }
+			.s, .down  { app.move(.down) }
+			.d, .right { app.move(.right) }
+			else {}
+		}
+	}
+	// eprintln('app.board.points: $app.board.points')
+}
+
+fn (mut app App) handle_swipe(start, end Pos) {
+	dx := end.x - start.x
+	dy := end.y - start.y
+	adx := abs(dx)
+	ady := abs(dy)
+	dmax := max(adx, ady)
+	dmin := min(adx, ady)
+
+	// Swipe was too short
+	if dmax < min_swipe_distance { return }
+	// Diagonal swipe, don't move
+	if dmax / dmin < 2 { return }
+
+	if adx > ady {
+		if dx < 0 { app.move(.left) } else { app.move(.right) }
+	} else {
+		if dy < 0 { app.move(.up) } else { app.move(.down) }
+	}
+}
